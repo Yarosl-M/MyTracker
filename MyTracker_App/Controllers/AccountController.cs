199f51b8c49cd5cc3c532503c6000e559dc998d7
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2025-07-05 20:51:09 +0300

    Add first login partial variant
    also account controller
    anyway, the partial isn't visible but does show up and get rendered

---
+++ MyTracker_App/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyTracker_App.ViewModels.Auth;
+
+namespace MyTracker_App.Controllers
+{
+    public class AccountController : Controller
+    {
+        public async Task<IActionResult> Login()
+        {
+            return Content("Login");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            return Content("Logout");
+        }
+
+        public async Task<IActionResult> SignUp()
+        {
+            return Content("SignUp");
+        }
+    }
+}
