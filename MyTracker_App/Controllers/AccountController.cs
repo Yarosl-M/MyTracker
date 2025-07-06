b642d763e400721bac8f53531fda1aa65298242d
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2025-07-06 13:55:45 +0300

    Add account layout and login page

--- MyTracker_App/Controllers/AccountController.cs
+++ MyTracker_App/Controllers/AccountController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyTracker_App.Models.Auth;
 using MyTracker_App.ViewModels.Auth;
+using System.Security.Claims;
 
 namespace MyTracker_App.Controllers
 {
-    public class AccountController : Controller
+    public class AccountController(SignInManager<User> _signInManager,
+        UserManager<User> _userManager, RoleManager<MyTrackerRole> _roleManager)
+        : Controller
     {
+        [HttpGet]
         public async Task<IActionResult> Login()
         {
+            return View();
             return Content("Login");
         }
 
