d7c4b5ee7ce0b38d08282d15c3fef1f567e4502a
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2025-07-11 14:06:11 +0300

    Add CreatedBy to a message model

---
+++ MyTracker_App/Migrations/20250711110428_Message_CreatedBy.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyTracker_App.Migrations
+{
+    /// <inheritdoc />
+    public partial class Message_CreatedBy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedById",
+                table: "Messages",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_CreatedById",
+                table: "Messages",
+                column: "CreatedById");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_AspNetUsers_CreatedById",
+                table: "Messages",
+                column: "CreatedById",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_AspNetUsers_CreatedById",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_CreatedById",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedById",
+                table: "Messages");
+        }
+    }
+}
--- MyTracker_App/Models/Domain/Message.cs
+++ MyTracker_App/Models/Domain/Message.cs
@@ -1,7 +1,10 @@
-﻿namespace MyTracker_App.Models.Domain
+﻿using MyTracker_App.Models.Auth;
+
+namespace MyTracker_App.Models.Domain
 {
     public class Message : Entry
     {
+        public User CreatedBy { get; set; }
         public required string Text { get; set; }
         public IList<Attachment> Attachments { get; set; }
         public Issue Issue { get; set; }
